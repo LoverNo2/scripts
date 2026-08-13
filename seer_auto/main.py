062be36592ef3ead8c71bede8fc14ac9a89d9b1c
wuxinhai <wu.xinhai@outlook.com>
2026-08-13 14:17:18 +0800

    fix(main): 修改jier模式的战斗次数为24
    将原有的255次战斗调整为24次，适配当前测试需求

--- seer_auto/main.py
+++ seer_auto/main.py
@@ -5,4 +5,4 @@ from battle.battle import battle
 if __name__ == "__main__":
     # battle("nail", times=96, heal_every=19)
     # battle("fire", times=255, heal_every=19)   
-    battle("jier", times=255, heal_every=19)   
+    battle("jier", times=24, heal_every=19)   
